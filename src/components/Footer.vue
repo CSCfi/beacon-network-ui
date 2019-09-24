a279d3e6bec462388b73451933948d4aa5818bc4
Teemu Kataja <teemu.kataja@csc.fi>
2019-09-24 11:56:08 +0300

    add new documents to footer, update contact email

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <footer class="footer">
+    <!-- Upper Footer -->
     <div class="container">
       <div class="content">
         <div class="columns">
@@ -9,9 +10,9 @@
             <p><a href="https://www.ga4gh.org/">GA4GH</a></p>
           </div>
           <div class="column">
-            <h5>Beacon Project</h5>
-            <p><a href="https://beacon-project.io/">Beacon Project</a></p>
-            <p><a href="https://ga4gh-discovery.github.io/networks/2018-12-13-network/">Beacon Network Project</a></p>
+            <h5>Legal</h5>
+            <p><router-link to="/tos">Terms of Service</router-link></p>
+            <p><router-link to="/privacy">Privacy Policy</router-link></p>
           </div>
           <div class="column">
             <h5>ELIXIR Beacon Network</h5>
@@ -19,13 +20,32 @@
             <p><router-link to="/docs">For Developers</router-link></p>
           </div>
           <div class="column">
-            <h5>Contact</h5>
-            <p>
-              CSC - IT Center for Science Ltd.<br>
-              Keilaranta 14, Espoo, Finland<br>
-              servicedesk@csc.fi<br>
-              <a href="https://www.csc.fi/">csc.fi</a>
-            </p>
+            <h5>Network Host</h5>
+            ELIXIR Beacon Network is provided by<br>
+            <a href="http://www.elixir-finland.org/en/frontpage/">ELIXIR Finland</a> · <a href="https://www.csc.fi/">CSC</a><br>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Lower Footer -->
+    <div class="container">
+      <div class="content">
+        <div class="columns">
+          <div class="column">
+            <h5>Beacon Project</h5>
+            <p><a href="https://beacon-project.io/">Beacon Project</a></p>
+            <p><a href="https://ga4gh-discovery.github.io/networks/2018-12-13-network/">Beacon Network Project</a></p>
+          </div>
+          <div class="column">
+            <!-- 2nd Column -->
+          </div>
+          <div class="column">
+            <!-- 3rd Column -->
+          </div>
+          <div class="column">
+            <h5>Network Contact</h5>
+            ELIXIR Europe<br>
+            <a href="mailto:bn-contact@elixir-europe.org">bn-contact@elixir-europe.org</a><br>
           </div>
         </div>
       </div>
