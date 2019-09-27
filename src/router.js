83f0b367ac17a5d99f3ac0221084c27e9ef0daea
Teemu Kataja <teemu.kataja@csc.fi>
2019-09-27 12:33:43 +0300

    fix vue bug where changing views doesn't return user to top of page, instead shows bottom of page

--- src/router.js
+++ src/router.js
@@ -7,6 +7,9 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+  },
   routes: [
     {
       path: "/",
