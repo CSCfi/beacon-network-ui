61840dede96154854291f48031dff3bd8db8020a
Teemu Kataja <teemu.kataja@csc.fi>
2019-10-07 09:59:53 +0300

    update about content

--- src/components/HomeTabs.vue
+++ src/components/HomeTabs.vue
@@ -7,28 +7,38 @@
 
       <b-tab-item label="About Beacons">
         <div class="columns content">
-          <div class="column word-wrap smaller">
+          <div class="column word-wrap">
             <h4>Beacon</h4>
-            A Beacon is an online web service that allows users to query an
-            institution’s databases, to determine whether they contain a genetic
-            variant of interest. The query is structured as a yes/no question of
-            the form: “Do you have any genomes with an ‘X’ at position Y on
-            chromosome Z?”<br />
-            <a href="https://beacon-project.io/"
-              >More information regarding Beacons</a
-            >
+            <p>
+              Beacons are web services that can be used to find genomic datasets
+              from across the world. Institutions, universities and biobanks can
+              share their data for secondary use by granting access to
+              researchers that have found an interesting variant within a
+              dataset. In short, Beacon is a search service for genomic data.
+            </p>
+            <p>
+              <a href="https://beacon-project.io/"
+                >More information regarding Beacons</a
+              >
+            </p>
           </div>
 
-          <div class="column word-wrap smaller">
+          <div class="column word-wrap">
             <h4>Beacon Network</h4>
-            Beacon networks consist of registries of multiple Beacons, in
-            different locations, which can be queried through a common intercace
-            or gateway protocol. The ELIXIR Beacon Network is a collection of
-            Beacons from ELIXIR nodes.<br />
-            <a
-              href="https://ga4gh-discovery.github.io/categories/discovery_networks.html"
-              >More information regarding Beacon Networks</a
-            >
+            <p>
+              Beacon Network is a collection of Beacons that have joined
+              together under a centralised gateway for easier access. Beacons
+              that have been connected to the network can be asynchronously
+              queried from the user interface. Beacon Network provides a wider
+              pool of data for researchers to sift through. The ELIXIR Beacon
+              Network is a collection of Beacons from the ELIXIR Nodes.
+            </p>
+            <p>
+              <a
+                href="https://ga4gh-discovery.github.io/categories/discovery_networks.html"
+                >More information regarding Beacon Networks</a
+              >
+            </p>
           </div>
         </div>
       </b-tab-item>
@@ -38,20 +48,27 @@
           <div class="column">
             <div class="word-wrap">
               <h4>Basic Search</h4>
-              The Beacon Network searchbar is a simple form with two options:
-              selection of assembly and definition of variant. The assembly can
-              be selected from the dropdown menu on the left side, currently
-              supporting assemblies GRCh37 and GRCh38. The searchbar then takes
-              the desired variant information as a search term which will be
-              queried for in the dataset database.
+              <p>
+                The Beacon Network searchbar is a simple form with two options:
+                selection of assembly and definition of variant. The assembly
+                can be selected from the dropdown menu on the left side. The
+                searchbar then takes the desired variant information as a search
+                term which will be sent to Beacons in a structured manner.
+              </p>
             </div>
             <div class="word-wrap">
               <h4>Basic Search Structure</h4>
-              The search structure follows a strict convention of {CHROMOSOME} :
-              {CHROMOSOMAL POSITION} {REFERENCE BASES} > {ALTERNATE BASES |
-              VARIANT TYPE}. Note that Beacon Network uses an adaptive
-              coordinate system (0-1-based), while Beacon APIs use a 0-based
-              (interbase) coordinate system.
+              <p>
+                The search structure follows a strict convention of:
+              </p>
+              <pre>Chromosome : Position ReferenceBase > AlternateBase</pre>
+              <p>
+                and
+              </p>
+              <pre>Chromosome : Position ReferenceBase > VarianType</pre>
+              <p>
+                Beacon Network uses an adaptive coordinate system (0-1-based).
+              </p>
             </div>
             <div class="word-wrap">
               <h4>Advanced Search</h4>
@@ -62,28 +79,40 @@
           <div class="column">
             <div class="word-wrap">
               <h4>Example Queries</h4>
-              <b>Example query:</b> I want to find a genomic dataset which has
-              samples of insertion type variants in chromosome 1 at position
-              104431390 where the reference base is C. This would transform into
-              the following statement in the searchbar: 1 : 104431390 C > INS.
+              <h5>Example query:</h5>
+              <p>
+                I want to find a genomic dataset which has samples of insertion
+                type variants in chromosome 1 at position 104431390 where the
+                reference base is C. This would transform into the following
+                statement in the searchbar:
+              </p>
+              <pre>1 : 104431390 C > INS</pre>
             </div>
             <div class="word-wrap">
-              <b>Example query:</b> I want to find a genomic dataset which has
-              samples of single nucleotide polymorphism type of variant in the
-              mitochondrial chromosome at position 7600 where the reference base
-              is G and the alternate base is A. This would transform into the
-              following statement in the searchbar: MT : 7600 G > A.
+              <h5>Example query:</h5>
+              <p>
+                I want to find a genomic dataset which has samples of single
+                nucleotide polymorphism type of variant in the mitochondrial
+                chromosome at position 7600 where the reference base is G and
+                the alternate base is A. This would transform into the following
+                statement in the searchbar:
+              </p>
+              <pre>MT : 7600 G > A</pre>
             </div>
             <div class="word-wrap">
-              <b>Example query:</b> I want to find a genomic dataset which has
-              samples of multiple nucleotide polymorphism type of variant in the
-              mitochondrial chromosome at position 195 where the reference base
-              is TTACTAAAGT and the alternate base is of same length, ending in
-              GT, but having any variations of bases in the preceding bases. In
-              such cases, where the bases are unknown, we can use a wildcard N
-              to denote the existence of an unknown nucleotide. This would
-              transform into the following statement in the searchbar: MT : 195
-              TTACTAAAGT > NNNNNNNNGT.
+              <h5>Example query:</h5>
+              <p>
+                I want to find a genomic dataset which has samples of multiple
+                nucleotide polymorphism type of variant in the mitochondrial
+                chromosome at position 195 where the reference base is
+                TTACTAAAGT and the alternate base is of same length, ending in
+                GT, but having any variations of bases in the preceding bases.
+                In such cases, where the bases are unknown, we can use a
+                wildcard N to denote the existence of an unknown nucleotide.
+                This would transform into the following statement in the
+                searchbar:
+              </p>
+              <pre>MT : 195 TTACTAAAGT > NNNNNNNNGT</pre>
             </div>
           </div>
         </div>
@@ -118,9 +147,5 @@ export default {
   max-width: 80ch;
   padding-bottom: 25px;
   text-align: left;
-}
-
-.word-wrap.smaller {
-  max-width: 75ch;
 }
 </style>
