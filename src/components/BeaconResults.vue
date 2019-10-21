73ea32d9f59b78168ff140898bc02bf43d8bb1ac
Teemu Kataja <teemu.kataja@csc.fi>
2019-10-21 08:07:23 +0300

    wip detailed results

--- src/components/BeaconResults.vue
+++ src/components/BeaconResults.vue
@@ -178,6 +178,7 @@ export default {
   },
   methods: {
     sortNumbers(a, b, isAsc) {
+      console.log(a, b)
       if (isAsc) {
         return (
           a.datasetAlleleResponses.length > b.datasetAlleleResponses.length
