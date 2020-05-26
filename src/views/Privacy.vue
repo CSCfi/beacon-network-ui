6bbba705001721d4cfe9bfe96edff7514eae0623
teemu.kataja <teemu.kataja@csc.fi>
2020-05-26 14:28:20 +0300

    add matomo tracking

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -29,30 +29,65 @@
     <h3>Cookies</h3>
     <p>
       The service utilises cookies for servicing broader access to authenticated
-      and authorised users. If a user authenticates themselves at ELIXIR AAI,
-      their ELIXIR AAI Access Token (JWT) will be saved to cookies for a
-      duration of up to one hour. This cookie contains data such as what is the
-      user's ELIXIR ID. This cookie is only available from the domain of the
-      service, and can only travel in encrypted HTTPS traffic. Other cookies are
-      also utilised, but they hold no personal data. Users are advised to delete
-      their browser session cookies if they are using the service on a public
-      computer.
+      and authorised users, and for anonymous page view tracking. If a user
+      authenticates themselves at ELIXIR AAI, their ELIXIR AAI Access Token
+      (JWT) will be saved to cookies for a duration of up to one hour. This
+      cookie contains data such as what is the user's ELIXIR ID. This cookie is
+      only available from the domain of the service, and can only travel in
+      encrypted HTTPS traffic. Other cookies are also utilised, but they hold no
+      personal data. Users are advised to delete their browser session cookies
+      if they are using the service on a public computer.
     </p>
     <p>
       Cookies used by this service:
       <br /><b>Cookies for all users:</b> <br /><i>elixir-cookies</i>: disables
       the Privacy Policy and Terms of Service notification, if user closes the
-      notification. <br /><b>Cookies for authenticated users:</b> <br /><i
-        >oidc_state</i
-      >: A session key used in the authentication process at ELIXIR AAI.
-      Contains no personal or identifiable data. <br /><i>logged_in</i>:
-      Indicates that the user has authenticated. Contains no personal or
-      identifiable data. <br /><i>bona_fide</i>: Indicates the user's Bona Fide
-      status. Contains no personal or identifiable data. <br /><i
-        >access_token</i
-      >: ELIXIR AAI JWT access token, is used to gain access to permitted
-      datasets at Beacons. Contains user's ELIXIR ID.
+      notification. <br />
+      <i>mtm_consent</i>: enables anonymous page view tracking with Matomo
+      software hosted by CSC at ELIXIR Finland.<br />
+      <i>elixir-consent-given</i>: for managing Matomo consent on UI side.<br />
+      <b>Cookies for authenticated users:</b> <br /><i>oidc_state</i>: A session
+      key used in the authentication process at ELIXIR AAI. Contains no personal
+      or identifiable data. <br /><i>logged_in</i>: Indicates that the user has
+      authenticated. Contains no personal or identifiable data. <br /><i
+        >bona_fide</i
+      >: Indicates the user's Bona Fide status. Contains no personal or
+      identifiable data. <br /><i>access_token</i>: ELIXIR AAI JWT access token,
+      is used to gain access to permitted datasets at Beacons. Contains user's
+      ELIXIR ID.
     </p>
+    <h3>Tracking</h3>
+    <p>
+      The service utilises anonymous page view tracking for evaluating traffic
+      to the website, and its level of use. Tracking is conducted with
+      <a href="https://matomo.org/">Matomo web analytics software</a> hosted by
+      CSC (ELIXIR Finland). The server and data are hosted and kept in Finland
+      (EU), and the raw data is not shared outside of the organisation. IP
+      addresses are anonymised by setting the last two digits to 0, e.g. the IP
+      address <i>193.167.189.101</i> is logged as <i>193.167.0.0</i>. General
+      quarterly and yearly report data on page views may be shared with ELIXIR
+      partners.
+    </p>
+    <p>
+      CSC (ELIXIR Finland) acts as the data owner and data handler for ELIXIR
+      Beacon Network traffic data.<br />
+      <a href="https://www.csc.fi/en/privacy">CSC Privacy Policy</a>
+    </p>
+    <p>
+      Tracking can be disabled by clicking the button below. Alternatively, you
+      can set your browser to send a <i>Do Not Track</i> message, and Matomo
+      software will take this into account.
+    </p>
+    <div v-if="getCookie('elixir-tracking-consent') == 'true'">
+      <b-button v-on:click="disableTracking()" type="is-warning"
+        >Disable Tracking</b-button
+      >
+    </div>
+    <div v-else>
+      <b-button v-on:click="enableTracking()" type="is-warning"
+        >Enable Tracking</b-button
+      >
+    </div>
     <h3>Security</h3>
     <p>
       The service and all of its components will always be served over encrypted
@@ -74,7 +109,7 @@
     <h3>Changes to Privacy Policy</h3>
     <p>
       This document may be updated at any time without given notice.
-      <br /><i>Last updated: 27 September 2019.</i>
+      <br /><i>Last updated: 26 May 2020.</i>
     </p>
     <h3>Contact Us</h3>
     <p>
@@ -86,6 +121,38 @@
     <router-link to="/tos">Terms of Service</router-link><br />
   </div>
 </template>
+
+<script>
+import VueCookies from "vue-cookies";
+export default {
+  methods: {
+    enableTracking: function() {
+      this.$matomo.setConsentGiven();
+      VueCookies.set("elixir-tracking-consent", "true", Infinity);
+    },
+    disableTracking: function() {
+      this.$matomo.forgetConsentGiven();
+      VueCookies.set("elixir-tracking-consent", "false", Infinity);
+    },
+    getCookie: function(cname) {
+      // Function from https://www.w3schools.com/js/js_cookies.asp
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(";");
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+  }
+};
+</script>
 
 <style lang="scss" scoped>
 .word-wrap {
