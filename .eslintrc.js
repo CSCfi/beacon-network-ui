d2ea1cc97067916d4426b774f9852af1309b06ba
ViMuilu <vmuilu@gmail.com>
2021-06-08 11:02:43 +0300

    added typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: ["plugin:vue/essential", "prettier"],
+
   rules: {
     "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: '@typescript-eslint/parser'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    'prettier',
+    '@vue/typescript'
+  ]
 };
