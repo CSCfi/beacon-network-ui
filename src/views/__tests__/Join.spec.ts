3375b3e8f2c47b24c502b2d292061aff70432bbb
ViMuilu <vmuilu@gmail.com>
2021-06-10 16:13:48 +0300

    updates to join tests and package.json script

--- src/views/__tests__/Join.spec.ts
+++ src/views/__tests__/Join.spec.ts
@@ -1,23 +1,39 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
-import VueRouter from 'vue-router'
-import Join from '@/views/Join.vue'
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+import VueRouter from "vue-router";
+import Join from "@/views/Join.vue";
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
-const router = new VueRouter()
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const router = new VueRouter();
 
 const wrapper = mount(Join, {
-    localVue,
-    router
-})
+  localVue,
+  router,
+});
 
-describe('Join.vue', () => {
-    it('"Return button works', async () => {
-     const returnButton = wrapper.find('#returnButton');
-  
-     expect(returnButton.exists()).toBe(true)
-     returnButton.trigger('click');
-     await wrapper.vm.$nextTick();
-     expect(wrapper.vm.$route.fullPath).toBe("/") 
-    })
-})
+describe("Join.vue", () => {
+  it("URL input works", async () => {
+    const urlInput = wrapper.find("#url");
+
+    expect(urlInput.exists()).toBe(true);
+    urlInput.setValue("test");
+    await wrapper.vm.$nextTick();
+    expect((urlInput.element as HTMLInputElement).value).toBe("test");
+  });
+  it("apiKey input works", async () => {
+    const apiKey = wrapper.find("#apiKey");
+
+    expect(apiKey.exists()).toBe(true);
+    apiKey.setValue("test");
+    await wrapper.vm.$nextTick();
+    expect((apiKey.element as HTMLInputElement).value).toBe("test");
+  });
+  it('"Return button works', async () => {
+    const returnButton = wrapper.find("#returnButton");
+
+    expect(returnButton.exists()).toBe(true);
+    returnButton.trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.$route.fullPath).toBe("/");
+  });
+});
