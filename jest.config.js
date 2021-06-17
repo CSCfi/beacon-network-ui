508cdc57e794dbc44f7be53147a5ae42b1200884
ViMuilu <vmuilu@gmail.com>
2021-06-17 15:15:48 +0300

    fix to jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  setupFilesAfterEnv: ["<rootDir>/tests/unit/vueSetup.js"]
+  setupFilesAfterEnv: ["<rootDir>/tests/unit/vueSetup.js"],
+  coverageReporters: ["text", "html"]
 };
