9c104a2788de5d978a7ecfdf9fcbfd939c5cbafa
ViMuilu <vmuilu@gmail.com>
2021-07-02 13:34:17 +0300

    updated e2e actions

--- src/components/beaconDatasets.vue
+++ src/components/beaconDatasets.vue
@@ -1,16 +1,20 @@
 <template>
-    <section v-on:load="queryAPI" title="List of beacons and their datasets">
-        <h1>Datasets</h1>
-        
-          <li v-on:click="setDisplay(beacon)" v-for="beacon in beaconsAndDataSets" :key="beacon.id">
-              <a>{{ beacon.beaconName }}</a>
-              <ul v-if= beacon.active>
-                  <li class="indented" v-for="dataset in beacon.datasets" :key="dataset">
-                  {{ dataset }}
-                  </li>
-              </ul>
-          </li>
-        
+  <section v-on:load="queryAPI" title="List of beacons and their datasets">
+    <h1>Datasets</h1>
+
+    <li
+      v-on:click="setDisplay(beacon)"
+      v-for="beacon in beaconsAndDataSets"
+      :key="beacon.id"
+    >
+      <a>{{ beacon.beaconName }}</a>
+      <ul v-if="beacon.active">
+        <li class="indented" v-for="dataset in beacon.datasets" :key="dataset">
+          {{ dataset }}
+        </li>
+      </ul>
+    </li>
+
     <div class="tile is-ancestor" style="text-align:center" v-if="error">
       <p style="margin:auto">
         {{ error }}
@@ -26,7 +30,7 @@ export default {
   data() {
     return {
       beacons: [],
-      beaconsAndDataSets:[],
+      beaconsAndDataSets: [],
       error: "",
       registry: process.env.VUE_APP_REGISTRY_URL,
       active: false
@@ -34,65 +38,61 @@ export default {
   },
   methods: {
     queryBeaconAPI: async function() {
-        var vm = this;
-        
-        for (const beacon of vm.beacons){
-            const beaconData = {
-                beaconName: beacon.name,
-                datasets: [],
-                active: false
-            };
-            axios
-            .get(beacon.url)
-            .then(response => {
-                const  datasets = []
-                for (const data of response.data.datasets){
-                    datasets.push(data.id);
-                }
-                beaconData.datasets = datasets
-                
-            })
-            .catch(error => {
-                beaconData.datasets = ["Unable to connect"]
-            });
-            vm.beaconsAndDataSets.push(beaconData)
-        }
-    },  
-    queryAPI:  function() {
+      var vm = this;
+
+      for (const beacon of vm.beacons) {
+        const beaconData = {
+          beaconName: beacon.name,
+          datasets: [],
+          active: false
+        };
+        axios
+          .get(beacon.url)
+          .then(response => {
+            const datasets = [];
+            for (const data of response.data.datasets) {
+              datasets.push(data.id);
+            }
+            beaconData.datasets = datasets;
+          })
+          .catch(error => {
+            beaconData.datasets = ["Unable to connect"];
+          });
+        vm.beaconsAndDataSets.push(beaconData);
+      }
+    },
+    queryAPI: function() {
       var vm = this;
       vm.beacons = []; // Clear view
       var url = `${vm.registry}services?type=beacon`;
-      console.log(url)
+      console.log(url);
       axios
         .get(url)
         .then(response => {
-          console.log(response)
-            for (const data of response.data){
-                vm.beacons.push(data);
-            }
-        vm.queryBeaconAPI();
+          console.log(response);
+          for (const data of response.data) {
+            vm.beacons.push(data);
+          }
+          vm.queryBeaconAPI();
         })
-        .catch(error => {
-        });
-      
+        .catch(error => {});
     },
-    setDisplay(beacon){
-        beacon.active = !beacon.active
-      
+    setDisplay(beacon) {
+      beacon.active = !beacon.active;
     }
   },
   beforeMount() {
     this.queryAPI();
-    
-  },
+  }
 };
 </script>
 <style>
-
 .indented {
-    font-size: 14px;
-    margin-left: 40px;
-    list-style-type: circle;
+  font-size: 14px;
+  margin-left: 40px;
+  list-style-type: circle;
 }
-h1 {font-size: 150%;}
+h1 {
+  font-size: 150%;
+}
 </style>
