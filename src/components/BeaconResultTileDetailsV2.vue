2000a16334fd951db64bbf7ffb757654e063d383
ViMuilu <vmuilu@gmail.com>
2021-08-20 13:11:35 +0300

    changed results into tables

--- src/components/BeaconResultTileDetailsV2.vue
+++ src/components/BeaconResultTileDetailsV2.vue
@@ -16,85 +16,90 @@
       </b-button>
       <div v-if="display" class="details-rows">
         <b-table
-          :data="results"
+          :data="parsedResults"
           :striped="true"
           ref="table"
           class="column details-table"
           detailed
           @details-open="row => $buefy.toast.open(`Expanded ${row.id}`)"
         >
-          <b-table-column label="Id" field="id" v-slot="results">
-            <b-tag
-              class="accessibility-green-tag"
-              title="Dataset is in public access"
-              ><b>{{ getId(results) }}</b></b-tag
+          <b-table-column label="Id" field="id" v-slot="props">
+            <b-tag class="accessibility-green-tag"
+              ><b>{{ getId(props) }}</b></b-tag
             >
           </b-table-column>
           <b-table-column label="Access">
-            <template v-slot:default="results">
+            <template v-slot:default="parsedResults">
               <b-tag
                 class="accessibility-green-tag"
-                v-if="checkForPublicDatasets(results)"
+                v-if="checkForPublicDatasets(parsedResults)"
                 title="Dataset is in public access"
                 ><b>Public</b></b-tag
               >
               <b-tag
                 class="accessibility-yellow-tag"
-                v-else-if="checkForRegisteredDatasets(results.row)"
+                v-else-if="checkForRegisteredDatasets(parsedResults)"
                 title="Dataset requires ELIXIR Bona Fide status to access"
                 ><b>Registered</b></b-tag
               >
               <b-tag
                 class="accessibility-red-tag"
-                v-else-if="checkForControlledDatasets(results.row)"
+                v-else-if="checkForControlledDatasets(parsedResults)"
                 title="Dataset requires permissions from data owner to access"
                 ><b>Controlled</b></b-tag
               >
               <b-tag class="access-tag" type="is-light" v-else>Unknown</b-tag>
             </template>
           </b-table-column>
-          <template #detail="results">
+          <template #detail="parsedResults">
             <article class="media">
               <div class="media-content">
                 <div class="content">
-                  <ul v-for="(result, index) in results" :key="index">
-                    <ul
-                      id="resultContent"
-                      v-for="(content, index) in result"
-                      :key="index"
-                    >
-                      {{
-                        index + ": "
-                      }}
-                      <span v-if="checkIfObject(content)">
-                        {{ content }}
-                      </span>
-                      <ul v-else v-for="(content, indx) in content" :key="indx">
-                        <span v-if="checkIfObject(content)">
-                          {{ indx + ": " }}
-                          {{ content }}
-                        </span>
-                        <div v-else v-for="(cont, ind) in content" :key="ind">
-                          <span v-if="checkIfObject(cont)">
-                            {{ ind + ": " + cont }}
-                          </span>
-                          <div v-else v-for="(con, inx) in cont" :key="inx">
-                            <span v-if="checkIfObject(con)">
-                              {{ inx + ": " + con }}
-                            </span>
-                            <div v-else v-for="(co, ix) in con" :key="ix">
-                              <span v-if="checkIfObject(co)">
-                                {{ ix + ": " + co }}
-                              </span>
-                              <div v-else v-for="(c, i) in co" :key="i">
-                                {{ i + ": " + c }}
+                  <b-table
+                    :data="objectToArray(parsedResults)"
+                    ref="table"
+                    class="column details-table"
+                    detailed
+                    @details-open="
+                      row => $buefy.toast.open(`Expanded ${row.id}`)
+                    "
+                  >
+                    <b-table-column v-slot="props">
+                      <b>{{ props.row[0] }}</b>
+                    </b-table-column>
+                    <template #detail="props">
+                      <div v-if="typeof props.row[1] == 'string'">
+                        {{ props.row[1] }}
+                      </div>
+                      <div v-if="typeof props.row[1] == 'object'">
+                        <ul v-for="(prop, index) in props.row[1]" :key="index">
+                          <b v-if="typeof index == 'string'"> {{ index }}: </b>
+                          <b v-if="prop == null">
+                            No data
+                          </b>
+                          <b v-if="typeof prop != 'object'">
+                            {{ prop }}
+                          </b>
+                          <div v-else class="listOfItems">
+                            <li v-for="(item, index) in prop" :key="index">
+                              {{ index }}:
+                              <b v-if="typeof item != 'object'">
+                                {{ item }}
+                              </b>
+                              <div v-else class="listOfItems">
+                                <li v-for="(it, index) in item" :key="index">
+                                  {{ index }}: {{ it }}
+                                </li>
                               </div>
-                            </div>
+                            </li>
                           </div>
-                        </div>
-                      </ul>
-                    </ul>
-                  </ul>
+                        </ul>
+                      </div>
+                      <div v-if="props.row[1] == null">
+                        No data
+                      </div>
+                    </template>
+                  </b-table>
                 </div>
               </div>
             </article>
@@ -114,7 +119,7 @@ export default {
       display: false
     };
   },
-  watch: {},
+  computed: {},
   methods: {
     getId: function(results) {
       if (Object.keys(results.row)[0] != "variant") {
@@ -128,6 +133,7 @@ export default {
         return "variants id: " + results.row.variant.variantId;
       }
     },
+    getIndexById(id) {},
     checkObjectLenght: function(item) {
       if (item == null || item.lenght == 0) {
         return false;
@@ -148,35 +154,57 @@ export default {
       else this.display = true;
     },
     checkForPublicDatasets: function(result) {
-      if (
-        result.info &&
-        result.info.accessType &&
-        result.info.accessType == "PUBLIC"
-      )
-        return true;
+      // commented to reduce error messages FIX LATER
+      /*       console.log(result);
+      if (result.row != null) {
+        if (
+          result.row.info &&
+          result.row.info.accessType &&
+          result.row.info.accessType == "PUBLIC"
+        )
+          return true;
+      }
+      return false; */
     },
     checkForRegisteredDatasets: function(result) {
-      if (
-        result.info &&
-        result.info.accessType &&
-        result.info.accessType == "REGISTERED"
-      )
-        return true;
+      /*       if (result.row != null) {
+        if (
+          result.row.info &&
+          result.row.info.accessType &&
+          result.row.info.accessType == "REGISTERED"
+        )
+          return true;
+      }
+      return false; */
     },
     checkForControlledDatasets: function(result) {
-      if (
-        result.info &&
-        result.info.accessType &&
-        result.info.accessType == "CONTROLLED"
-      )
-        return true;
+      /* if (result.row != null) {
+        if (
+          result.row.info &&
+          result.row.info.accessType &&
+          result.row.info.accessType == "CONTROLLED"
+        )
+          return true;
+      }
+      return false; */
+    },
+    parseResultsToArray: function() {
+      this.parsedResults = JSON.parse(JSON.stringify(this.$props.results));
+    },
+    objectToArray: function(object) {
+      return Object.entries(object.row);
     }
   },
-  beforeMount() {}
+  beforeMount() {
+    this.parseResultsToArray();
+  }
 };
 </script>
 
 <style scoped>
+.listOfItems {
+  margin-left: 50px;
+}
 #resultContent {
   border-bottom: 1px solid black;
 }
