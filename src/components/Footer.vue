3c80f69e0a8f5cd6807c81726477d253b29b6182
Stefan Negru <stefan@blankdots.com>
2021-12-07 11:46:23 +0200

    show current year in copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -142,7 +142,7 @@
               >
             </p>
             <p class="legal">
-              Copyright © ELIXIR 2019 |
+              Copyright © ELIXIR 2019 - {{currentYear}} |
               <router-link
                 to="/tos"
                 title="Read the Terms of Service of ELIXIR Beacon Network"
@@ -165,6 +165,7 @@
 <script>
 export default {
   name: "footer-bar",
+  currentYear: new Date().getFullYear(),
 };
 </script>
 
